08f20ffa76fab4baad85391515c2efdc4330307b
Filip Walter <filipwalter10@gmail.com>
2026-09-11 13:55:16 +0200

    ändrade om return i createCustomer skickar istället för ett tomt objekt ett exception som hanteras i Hotelbooking

--- src/main/java/org/example/customerservice/customer/service/CustomerService.java
+++ src/main/java/org/example/customerservice/customer/service/CustomerService.java
@@ -5,9 +5,11 @@ import org.example.customerservice.customer.model.*;
 import org.example.customerservice.customer.repository.CustomerRepository;
 import org.example.customerservice.utils.encoder.Encoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
 
 @Service
 public class CustomerService {
@@ -17,28 +19,30 @@ public class CustomerService {
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
-
     public CustomerResponse createCustomer(CreateCustomerRequest request) {
-        try {
-            List<Customer> list= customerRepository.getCustomerByEmail(request.email());
-            if(list.isEmpty()){
-                Customer customer = new Customer();
 
-                customer.setFirstName(request.firstName());
-                customer.setLastName(request.lastName());
-                customer.setEmail(request.email());
-                customer.setPhoneNumber(request.phoneNumber());
-                customer.setPassword(Encoder.hashPassword(request.password()));
+            List<Customer> list =
+                    customerRepository.getCustomerByEmail(request.email());
 
-                Customer savedCustomer = customerRepository.save(customer);
-
-                return toResponse(savedCustomer);
+            if(!list.isEmpty()){
+                throw new ResponseStatusException(
+                        CONFLICT,
+                        "Epostadressen är redan registrerad"
+                );
             }
-        }catch (ResourceAccessException e) {
-            return new CustomerResponse(null, null, null, null, null);
-        }
-        return new CustomerResponse(null, null, null, null, null);
+
+            Customer customer = new Customer();
+
+            customer.setFirstName(request.firstName());
+            customer.setLastName(request.lastName());
+            customer.setEmail(request.email());
+            customer.setPhoneNumber(request.phoneNumber());
+            customer.setPassword(Encoder.hashPassword(request.password()));
+
+            return toResponse(customerRepository.save(customer));
+
     }
+
     public CustomerResponse getCustomerById(Long id) {
         Customer customer = customerRepository.findById(id).orElseThrow(() -> new RuntimeException("Kunden finns inte"));
 
